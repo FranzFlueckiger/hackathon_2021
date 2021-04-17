8d07e117d99a39b3203a6ff416203a1342b78b8f
FranzFlueckiger <fluecfra@students.zhaw.ch>
2021-04-17 10:05:31 +0200

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import { AppBar, Tabs, Tab } from "@material-ui/core";
+import React from "react";
 import "./App.css";
 import { country_mappings } from "./data/countriy_mappings";
 import { data } from "./data/processed_hackathon_data";
 
 function App() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: any, newValue: any) => {
+    setValue(newValue);
+  };
+
   return (
     <>
+      <AppBar position="static">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="simple tabs example"
+        >
+          <Tab label="Item One" />
+          <Tab label="Item Two" />
+          <Tab label="Item Three" />
+        </Tabs>
+      </AppBar>
+
       {console.log(data)}
       {country_mappings}
     </>
