f7e02eb461df8a0995a7b8b5bc098b0be9e46283
FranzFlueckiger <fluecfra@students.zhaw.ch>
2021-04-17 11:37:05 +0200

    Implement country mapping

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,8 +1,30 @@
 import Box from "@material-ui/core/Box";
+import { country_mappings } from "../data/countriy_mappings";
 import { data } from "../data/processed_hackathon_data";
 
 export function MapView(props: any) {
   const { value, index } = props;
+
+  const countryMappingsDef = country_mappings.map((countryMapping) => {
+    return {
+      countryName: countryMapping.Land.trim().toLowerCase(),
+      abbreviation: countryMapping.ISO_3166_1,
+    };
+  });
+
+  const mappedData = data.map((datum) => {
+    let cleanCountryName = datum["countries 0"].replace(/(^\s*,)|(,\s*$)/g, "");
+    cleanCountryName = cleanCountryName.replace(" (?)", "").toLowerCase();
+    const mapping = countryMappingsDef.find(
+      (mapping) => mapping.countryName === cleanCountryName
+    );
+    return {
+      data: datum,
+      mapping,
+    };
+  });
+
+  console.log(mappedData);
 
   return (
     <div
@@ -11,7 +33,7 @@ export function MapView(props: any) {
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
     >
-      {value === index && <Box p={3}>{data}</Box>}
+      {value === index && <Box p={3}></Box>}
     </div>
   );
 }
--- src/components/StoryView.tsx
+++ src/components/StoryView.tsx
@@ -1,8 +1,11 @@
 import Box from "@material-ui/core/Box";
 import { country_mappings } from "../data/countriy_mappings";
+import { data } from "../data/processed_hackathon_data";
 
 export function StoryView(props: any) {
   const { value, index } = props;
+
+  console.log(data);
 
   return (
     <div
@@ -11,7 +14,7 @@ export function StoryView(props: any) {
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
     >
-      {value === index && <Box p={3}>{country_mappings}</Box>}
+      {value === index && <Box p={3}></Box>}
     </div>
   );
 }
